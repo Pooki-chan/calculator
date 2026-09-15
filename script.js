08fd53d4bff2f51b15b75910925f983210b4f2bf
pooki-chan <groundmaker496@gmail.com>
2026-09-15 20:10:38 +0300

    made the basic calculation but no infinite subst

--- script.js
+++ script.js
@@ -1,45 +1,42 @@
-let operator;
-let numberOne;
-let numberTwo;
+let operator="";
 let operatorClicked=false;
+let numberOne=""; 
+let numberTwo="";
 let input;
 // give numbers to them and display and add an event listener to all boxex who are indexed here
 const cont = document.querySelector("#container");
+const display= document.querySelector(".box");
+
+
 for (let i = 0; i < 10; i++) {
     const num = document.createElement("div");
     num.classList.add("int");
     num.textContent = `${i}`;
     cont.appendChild(num);
-
-
     num.addEventListener("click",function (){
-    const display= document.querySelector(".box");
-    display.textContent+=`${i}`;
-
-    // these divs dont exist so does their index
-    input=display.textContent;
-    // separates numbers
-    if(operatorClicked=false){
-    numberOne=input;
-    }
-    else{
-    numberTwo=input;
-    }
+    updatingDex=`${i}`;
+    updateDisplay(updatingDex);
     });
-  
-
 }
 //on operator button click we assign it a number and then pass onto operate function
 
+function updateDisplay(value){
+display.textContent=value;
+// separate stuff
+if (operatorClicked==true){
+    numberTwo+=value;
+}
+else
+{
+    numberOne+=value;
+}
+}
 function assignOperator(i){
 operatorClicked=true;
-    operator=`${i}`;
-    operate(operator,numberOne,numberTwo);
+operator=`${i}`;
+operatorAss(operator);
 }
-
-
 const menuList=document.querySelectorAll(".menu");
-
 for (let i=0;i<menuList.length;i++)
 {
     menuList[i].addEventListener("click",function(event){
@@ -48,14 +45,9 @@ for (let i=0;i<menuList.length;i++)
     
 }
 
-
-
-
-    
-const display= document.querySelector(".box");
 //logic for operators
 function add(a,b){
-return a+b;
+return Number(a)+Number(b);
 }
 function substract(a,b){
 return a-b;
@@ -65,24 +57,28 @@ return a*b;
 function divide(a,b){
 return a/b;
 }
-
+let result;
 //operate function takes the operatir and gives it its function hte = should display result
-function operate(operator,numberOne,numberTwo){
-    switch (operator){
-        case '1':
+switch (val){
+        case '0':
+            operator="=";
             display.textContent+="+";
-            add(numberOne,numberTwo);break; 
+            result=add(numberOne,numberTwo);
+            break; 
+        case '1':
+            display.textContent+="-";break; 
         case '2':
-            substract(numberOne,numberTwo);break;
+            display.textContent+="*";break; 
         case '3':
-            multiply(numberOne,numberTwo);break;
-        case '4':
-            divide(numberOne,numberTwo);break;
-        case '5':
-            display.textContent=`test`;
-            console.log("test"); 
-            
+            display.textContent+="/";break; 
+        case '=':
+            display.textContent=result;
+}
+
+function operatorAss(val){
+        result=add(numberOne,numberTwo);
+       
             // testing the = and it needs to calculate stuff actually
-    }
 }
     
+
